17855417f1d2602c4fd3d2d6d5d2e7b2a5de22e9
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-27 18:41:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,9 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
   the_manifest = {
-    "whale bone corsets"
-    porcelain vases	
+    "whale bone corsets" => 5,
+    "porcelain vases"	=> 2,
+    "oil paintings" =>
 
   }
 
