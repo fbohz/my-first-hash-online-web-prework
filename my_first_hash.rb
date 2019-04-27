a3d4c794a00350a19d8af2bab07253fb59b03462
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-27 18:45:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,6 +30,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -40,7 +41,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
@@ -48,5 +50,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
